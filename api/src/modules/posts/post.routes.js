18adfe932845d5875c8df61b0f88abdbb4c6a3fc
Elisa Delaunay <delaunayelisa97@gmail.com>
2026-04-16 18:20:34 +0200

    Create get all, put and delete routes for posts

--- api/src/modules/posts/post.routes.js
+++ api/src/modules/posts/post.routes.js
@@ -14,8 +14,28 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.get('/', (req, res) => {
-    res.send("Blabla")
+router.get('/', async (req, res) => {
+    try {
+        let filter = {};
+
+        if (req.query.tag) {
+            filter.tags = req.query.tag;
+        }
+
+        if (req.query.author) {
+            filter.author = req.query.author;
+        }
+
+        if (req.query.date) {
+            filter.createdAt = { $gte: new Date(req.query.date)};
+        }
+
+        const posts = await Post.find(filter);
+
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({message: "Erreur lors de la récupération des articles", error: error.message});
+    }
 });
 
 router.get('/:id', async (req, res) => {
@@ -31,12 +51,42 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.put('/:id', (req, res) => {
-    res.send("Blabla")
+router.put('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            req.body,
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!updatedPost) {
+            return res.status(404).json({message: "Article introuvable"});
+        }
+
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        res.status(400).json({message: "Erreur lors de la modifcation de l'article", error: error.message})
+    };
 });
 
-router.delete('/:id', (req, res) => {
-    res.send("Blabla")
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deletedPost = await Post.findByIdAndDelete(id);
+
+        if (!deletedPost) {
+            return res.status(404).json({message: "Article introuvable"});
+        }
+
+        res.status(200).json({message: "L'article a bien été supprimé."});
+    } catch (error) {
+        res.status(500).json({ message: "Erreur lors de la suppression de l'article", error: error.message });
+    }
 });
 
 export default router;
